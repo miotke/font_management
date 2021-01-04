1f51d8b1c1948f4255f5bd2c3e56c580b43e10dc
Andrew Miotke <andrewmiotke@gmail.com>
2021-01-04 15:03:14 -0800

    Mac app: Starting the shell of the Mac app.
    - This commit creates and sets up a Mac Catalyst app using Swift.
    - Work done so far includes configuring the app to run without a
      storyboard and having to build the UI programmatically.

---
+++ Mac app/Fonts/Font List View/FontListViewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fonts
+//
+//  Created by Miotke, Andrew on 1/4/21.
+//
+
+import UIKit
+
+class FontListViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.blue
+    }
+}
+
